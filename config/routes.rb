7db247adc30f366b82ab977f181433d3f508bad1
ascko <rohde.adam@gmail.com>
2021-01-25 00:24:37 +0100

    added active_admin and resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
   root to: 'static_pages#home'
